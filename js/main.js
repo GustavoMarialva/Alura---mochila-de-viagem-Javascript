f1e2b8e459328e922165ed27ed87f58b533ae06f
Gustavo Marialva <gmarialva@gmail.com>
2023-02-02 17:44:22 -0300

    cria function criaElemento

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 const form = document.querySelector("#novoItem");
+const lista = document.getElementById("lista");
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
-  console.log(evento.target.elements["nome"].value);
-  console.log(evento.target.elements["quantidade"].value);
   //com evento.target foi possível adentrar no elements e verificar nome e quantidade. Sempre ver no console.log os elementos que serão referidos. Nesse caso nome e qt
+  criaElemento(
+    evento.target.elements["nome"].value,
+    evento.target.elements["quantidade"].value
+  );
 });
+
+function criaElemento(nome, quantidade) {
+  const novoItem = document.createElement("li");
+  novoItem.classList.add("item");
+
+  const numeroItem = document.createElement("strong");
+  numeroItem.innerHTML = quantidade;
+
+  novoItem.appendChild(numeroItem);
+  novoItem.innerHTML += nome;
+
+  lista.appendChild(novoItem);
+}
