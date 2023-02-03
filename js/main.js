1d39434d293ef091836f58913252cc0044e41f66
Gustavo Marialva <gmarialva@gmail.com>
2023-02-03 10:46:42 -0300

    adiciona localStorage, cria objeto e array item

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const form = document.querySelector("#novoItem");
 const lista = document.getElementById("lista");
+const itens = [];
 
 form.addEventListener("submit", (evento) => {
   evento.preventDefault();
   //com evento.target foi possível adentrar no elements e verificar nome e quantidade. Sempre ver no console.log os elementos que serão referidos. Nesse caso nome e qt
-  criaElemento(
-    evento.target.elements["nome"].value,
-    evento.target.elements["quantidade"].value
-  );
+  const nome = evento.target.elements["nome"];
+  const quantidade = evento.target.elements["quantidade"];
+
+  criaElemento(nome.value, quantidade.value);
+
+  nome.value = "";
+  quantidade.value = "";
 });
 
 function criaElemento(nome, quantidade) {
@@ -21,4 +25,13 @@ function criaElemento(nome, quantidade) {
   novoItem.innerHTML += nome;
 
   lista.appendChild(novoItem);
+
+  const itemAtual = {
+    nome: nome,
+    quantidade: quantidade,
+  };
+
+  itens.push(itemAtual);
+
+  localStorage.setItem("item", JSON.stringify(itens));
 }
