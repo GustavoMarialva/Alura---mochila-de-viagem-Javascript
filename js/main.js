9c8b6c97d44785db728f83f2f1d0650c5fc5eb68
Gustavo Marialva <gmarialva@gmail.com>
2023-02-03 15:45:08 -0300

    corrige array

--- js/main.js
+++ js/main.js
@@ -16,9 +16,19 @@ form.addEventListener("submit", (evento) => {
     quantidade: quantidade.value,
   };
 
-  criaElemento(itemAtual);
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
 
-  itens.push(itemAtual);
+  if (existe) {
+    itemAtual.id = existe.id;
+    atualizaElemento(itemAtual);
+
+    itens[existe.id] = itemAtual;
+  } else {
+    itemAtual.id = itens.length;
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual);
+  }
 
   localStorage.setItem("itens", JSON.stringify(itens));
 
@@ -32,9 +42,15 @@ function criaElemento(item) {
 
   const numeroItem = document.createElement("strong");
   numeroItem.innerHTML = item.quantidade;
+  numeroItem.dataset.id = item.id;
 
   novoItem.appendChild(numeroItem);
   novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade;
 }
